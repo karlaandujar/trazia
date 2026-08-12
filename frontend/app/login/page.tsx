aff4b1831a210550f4be2fd63a32c1eb924701f7
Karla Andujar <karlaandujar@vt.edu>
2026-08-11 21:29:47 -0400

    Adding why choose trazia section

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -104,7 +104,7 @@ export default function Login() {
                 </nav>
 
                 {/* Hero Section */}
-                <div className="flex justify-between gap-1 px-16 py-12 grid grid-cols-2 bg-[url('/trazia_hero_bg.png')] bg-position-[65%_20%]">
+                <div className="flex justify-between gap-1 px-16 py-12 grid grid-cols-2 bg-[url('/trazia_hero_bg.png')] bg-position-[65%_20%] border-b border-slate-300">
 
                     {/* Left column */}
                     <div className="col-start-1">
@@ -236,6 +236,59 @@ export default function Login() {
                 </div>
 
 
+                {/* Section below hero and login/signup cards */}
+                <div className="pb-7">
+                    <h3 className="text-2xl font-bold text-slate-800 mb-2 p-5">Why Choose Trazia</h3>
+                    <div className="flex justify-between px-5">
+                        {/* Feature 1: Save Time */}
+                            <div className="flex items-center">
+                                <div className="flex size-18 items-center justify-center rounded-md bg-[#E5F3FD]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-15">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                </div>
+
+                                <div className="pl-3">
+                                    <p className="font-semibold text-lg text-slate-700">Save Time</p>
+                                    <p className="text-gray-600 max-w-sm leading-tight">
+                                        Automate uploading hundreds of assignments into one place.
+                                    </p>
+                                </div>
+                            </div>
+
+                        {/* Feature 2: Stay Focused */}
+                            <div className="flex items-center">
+                                <div className="flex size-18 items-center justify-center rounded-md bg-[#E5F3FD]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-15">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                                    </svg>
+                                </div>
+                                
+                                <div className="pl-3">
+                                    <p className="font-semibold text-lg text-slate-700">Stay Focused</p>
+                                    <p className="text-gray-600 max-w-sm leading-tight">
+                                        Sort tasks by deadline, ensuring each one is hit on time.
+                                    </p>
+                                </div>
+                            </div>
+
+                        {/* Feature 3: Know Your Progress */}
+                            <div className="flex items-center">
+                                <div className="flex size-18 items-center justify-center rounded-md bg-[#E5F3FD]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-15">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+                                    </svg>
+                                </div>
+
+                                <div className="pl-3">
+                                    <p className="font-semibold text-lg text-slate-700">Know Your Progress</p>
+                                    <p className="text-gray-600 max-w-sm leading-tight">
+                                        Track your grades and see how you're doing in each class.
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
+                </div>
 
                 {showVerif && (
                     <div className="border p-4 bg-gray-100 max-w-md mx-auto shadow-lg w-full h-full">
