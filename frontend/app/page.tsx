5d7ad61d1bbf57294c7a975dd703e66e4f3fccdd
Karla Andujar <karlaandujar@vt.edu>
2026-08-11 23:19:29 -0400

    Redirecting to login page on startup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,46 +1,6 @@
 "use client";
-import { useState, useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  type Course = {
-    id: number;
-    course_number: string;
-    course_name: string;
-  };
-
-  const [courses, setCourses] = useState<Course[]>([]);
-  
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/courses")
-      .then((response) => response.json())
-      .then((data) => {
-        setCourses(data);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Courses</h1>
-
-      <div className="flex justify-center mt-8">
-        <table className="border-collapse border border-gray-400">
-          <thead>
-            <tr>
-              <th className="border border-gray-400 px-4 py-1">Course Number</th>
-              <th className="border border-gray-400 px-4 py-1">Course Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {courses.map((course) => (
-              <tr key={course.id}>
-                <td className="border border-gray-400 px-4 py-1">{course.course_number}</td>
-                <td className="border border-gray-400 px-4 py-1">{course.course_name}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+  redirect("/login");
 }
