5a2d9ca404b7cd607c2df898095b2a5032b070ac
Karla Andujar <karlaandujar@vt.edu>
2026-08-13 13:26:15 -0400

    Finishing up styling on dashboard stats

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -164,20 +164,77 @@ export default function Dashboard() {
                     </div>
                     
                     {/* Semester statistics */}
-                    <div className="flex">
-                        {/* Number of courses */}
-                        <div className="flex items-center justify-center bg-white rounded-xl p-4 pr-12 shadow-md">
-                            <div className="flex size-15 items-center justify-center rounded-full bg-[#DCEBF5] mr-5">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
-                                </svg>
+                    <div className="flex gap-[calc(3rem)]">
+                        <div className="h-45 flex gap-[calc(3rem)]">
+                            {/* Number of courses */}
+                            <div className="flex items-center justify-center bg-white rounded-xl p-10 pr-17 shadow-md w-75">
+                                <div className="flex size-15 items-center justify-center rounded-full bg-[#DCEBF5] mr-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                                    </svg>
+                                </div>
+
+                                <div>
+                                    <p className="text-2xl font-bold">{courses.length}</p>
+                                    <p className="text-slate-700 font-semibold">Courses</p>
+                                    <p className="text-slate-500">This semester</p>
+                                </div>
                             </div>
 
-                            <div>
-                                <p className="text-2xl font-bold">{courses.length}</p>
-                                <p className="text-slate-700 font-semibold">Courses</p>
-                                <p className="text-slate-500">This semester</p>
+                            {/* Number of assignments this week */}
+                            <div className="flex items-center justify-center bg-white rounded-xl p-10 pr-17 shadow-md">
+                                <div className="flex size-15 items-center justify-center rounded-full bg-[#DCEBF5] mr-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+                                    </svg>
+                                </div>
+
+                                <div>
+                                    <p className="text-2xl font-bold">{assignments.length}</p>
+                                    <p className="text-slate-700 font-semibold">Assignments</p>
+                                    <p className="text-slate-500">This week</p>
+                                </div>
                             </div>
+
+                            {/* Number of exams this week */}
+                            <div className="flex items-center justify-center bg-white rounded-xl p-10 pr-17 shadow-md w-65">
+                                <div className="flex size-15 items-center justify-center rounded-full bg-[#DCEBF5] mr-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-9">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                    </svg>
+                                </div>
+
+                                <div>
+                                    <p className="text-2xl font-bold">0</p>
+                                    <p className="text-slate-700 font-semibold">Exams</p>
+                                    <p className="text-slate-500">This week</p>
+                                </div>
+                            </div>
+                        </div>
+                        { /* Next up */ }
+                        <div className="bg-[#f7f9fa] rounded-xl min-w-[calc(100%-45rem)] min-h-[13rem] shadow-md">
+                            <div className="flex pt-5 pl-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-2 text-[#5775d6]">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+                                </svg>
+
+                                <p className="text-[#5775d6] text-xl font-semibold">Next up</p>
+                            </div>
+                            
+                            <p className="text-slate-700 text-2xl font-semibold pt-3 pl-4">assignment name</p>
+                            <p className="text-slate-700 text-sm pt-1 pl-4">course number • course name</p>
+
+                            <div className="flex pt-4 pl-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                                </svg>
+
+                                <p className="text-slate-700 text-sm pl-2">Due in _ days, DueDateHere</p>
+                            </div>
+
+                            <button className="border border-slate-300 min-w-[calc(100%-2rem)] text-[#5775d6] font-semibold mb-3 mt-7 py-2 px-4 rounded-lg hover:bg-[#e3eaff] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ml-4">
+                                View assignment <span className="text-lg">➜</span>
+                            </button>
                         </div>
                     </div>
 
