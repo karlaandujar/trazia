61ee48508b7a0ea76ec69077d4d5d097016966c8
Karla Andujar <karlaandujar@vt.edu>
2026-08-29 18:29:21 -0400

    Adding general styling to profile page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { getNav } from "../dashboard/page";
+
 
 export default function Profile(){
     const router = useRouter();
 
     return (
         <div>
-            <h1>profile page here</h1>
+            <div className="bg-white rounded-3xl shadow-sm overflow-hidden min-h-[calc(100vh-1rem)] max-w-[calc(100vw-1rem)]">
+                {/* Navigation bar */}
+                { getNav() }
+
+                <div className="flex gap-1 mx-2 px-16 pt-12 flex flex-col gap-8 bg-[url('/trazia_hero_bg.png')] min-h-[calc(100vh-7rem)] rounded-3xl bg-cover bg-position-[2%_20%]">
+                    {/* Headers and course addition button */}
+                    <div className="flex relative pt-6">
+                        <div>
+                            <h1 className="font-semibold text-3xl text-slate-800">My Profile</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
