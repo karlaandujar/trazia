09f15026ae340678c115bd8c7227486592f5aa3e
Karla Andujar <karlaandujar@vt.edu>
2026-09-16 09:16:10 -0400

    Adding basic profile picture addition form

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { getNav } from "../dashboard/page";
+import { useState } from "react";
 
 
 export default function Profile(){
     const router = useRouter();
+    const [showSetProfile, setShowSetProfile] = useState(false);
 
     return (
-        <div>
+        <div className={`${showSetProfile ? 'before:fixed before:absolute before:inset-0 before:z-50 before:bg-black/30' : '' }`}>
             <div className="bg-white rounded-3xl shadow-sm overflow-hidden min-h-[calc(100vh-1rem)] max-w-[calc(100vw-1rem)]">
                 {/* Navigation bar */}
                 { getNav() }
@@ -24,7 +26,7 @@ export default function Profile(){
                     <div className="ml-[calc(5%)] bg-white rounded-xl py-3 border border-slate-300">
                         {/* Profile pic and general information */}
                         <div className="flex">
-                            <img className="h-26 w-26 rounded-full cursor-pointer" src="/profile.png" alt="Profile"></img>
+                            <img className="h-26 w-26 rounded-full cursor-pointer" src="/profile.png" onClick={() => setShowSetProfile(true)} alt="Profile"></img>
                             <div className="ml-5">
                                 <h1>name</h1>
                                 <h1>email</h1>
@@ -32,6 +34,16 @@ export default function Profile(){
                                 <h1>major • class of 20xx</h1>
                             </div>
                         </div>
+
+                        {showSetProfile && (
+                            <div className="fixed inset-0 bg-opacity-20 flex items-center justify-center z-50">
+                                <div className="bg-white p-6 rounded-lg shadow-lg w-[calc(100%-2rem)] max-w-md">
+                                    <h2 className="text-xl font-semibold mb-4">Set Profile Picture</h2>
+                                    <input type="file" accept="image/*" className="mb-4" />
+                                </div>
+                                <p onClick={() => setShowSetProfile(false)} className="text-gray-500 hover:text-gray-700 text-size-lg cursor-pointer">x</p>
+                            </div>
+                        )}
 
                         {/* 2 cards for account info and preferences */}
                         <div className="flex justify-evenly mt-2 mb-1">
